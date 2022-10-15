680d06ac539594c55f4ba8f8f72725406d5463d2
Iamvictorsam <iamvictorsam@gmail.com>
2022-10-15 10:33:41 +0100

    setup complete

--- src/App.vue
+++ src/App.vue
@@ -10,12 +10,25 @@
 import Introduction from "./components/Introduction.vue";
 import Services from "./components/Services.vue";
 import Appreciation from "./components/Appreciation.vue";
+import LocomotiveScroll from "locomotive-scroll";
 
 export default {
   components: {
     Introduction,
     Services,
     Appreciation,
+  },
+  methods: {
+    setLocomotiveScroll() {
+      new LocomotiveScroll({
+        el: this.$refs.container,
+        smooth: true,
+        multiplier: 2,
+      });
+    },
+  },
+  mounted() {
+    this.setLocomotiveScroll();
   },
 };
 </script>
